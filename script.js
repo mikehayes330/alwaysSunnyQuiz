4706aed7f0962625f890e93e69790854ba27dfaf
Mike Hayes <mikehayes330@gmail.com>
2020-06-30 21:54:45 -0400

    fixed some functionality now some feature work

--- script.js
+++ script.js
@@ -1,120 +1,147 @@
-var startBtn = $("#startbtn")
-var btnA = $("#answer1")
-var btnB = $("#answer2")
-var btnC = $("#answer3")
-var btnD = $("#answer4")
-var display = $("#timeLeft")
-var questionBoxElement = $("#questionBox")
-var answersButtonElement = $("span")
+var startBtn = $("#startbtn");
+var btnA = $("#answer1");
+var btnB = $("#answer2");
+var btnC = $("#answer3");
+var btnD = $("#answer4");
+var display = $("#timeLeft");
+var questionBoxElement = $("#questionBox");
+var answersButtonElement = $("span");
 var score = 0;
-var currentQuestionIndex, shuffledQuestions;
-var startTime = 60 * 3
-
-   
-
-   // here are the event handlers
-  startBtn.on("click", startQuiz)
-  btnA.on("click", checkAnswer )
-  btnB.on("click", checkAnswer )
-  btnC.on("click", checkAnswer )
-  btnD.on("click", checkAnswer )
+var currentQuestionIndex = 0;
+var shuffledQuestions;
+var startTime = 60 * 3;
+var scoreElement = $('#score');
 
 
+// here are the event handlers
+startBtn.on("click", startQuiz);
+btnA.on("click", checkAnswer);
+btnB.on("click", checkAnswer);
+btnC.on("click", checkAnswer);
+btnD.on("click", checkAnswer);
 
-  // here is the timer function
-  function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.text(minutes + ":" + seconds);
+// start quiz function
+function startQuiz() {
+  $("#title").fadeOut(700);
+  $("#firstQuestion").delay(800).fadeIn(800);
+  var display = $("#timeLeft");
+  startTimer(startTime, display);
+  setNextQuestion();
+}
+console.log(display)
+function checkAnswer() {
+  if (this.innerText === questions[currentQuestionIndex].answer) {
+   // trying to change color on correct
+   // $("body").addClass("correct");
+    $(".container").fadeOut(400).fadeIn(800);
+    currentQuestionIndex ++;
+    alert("CORRECT!!")
+    setNextQuestion();
+    score ++;
+  } else {
+    // $('body').addClass('wrong');
+    $(".container").fadeOut(400).fadeIn(800);
+    currentQuestionIndex ++;
+    setNextQuestion();
+    alert("WRONG!!")
+    
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+  
+  }
 }
 
+function setNextQuestion() {
   
-  // start quiz function 
-  function startQuiz() {
-    $( "#title" ).fadeOut(700);
-    $( "#firstQuestion" ).delay(800).fadeIn(800);
-    var display = $("#timeLeft")
-    startTimer(startTime, display  );
-   shuffledQuestions = questions.sort(() => Math.random() - .5)
-   currentQuestionIndex = 0;
-    setNextQuestion();
-    
-   
-    
-  
-  };
+  showQuestion();
+  showAnswers();
+  displayScore();
 
-  function setNextQuestion(){
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-    showAnswers(shuffledQuestions[currentQuestionIndex]);
-    
-  };
+}
 
+var questions = [
+  {
+    title: "What is Charlie's Job?",
+    choices: ["Janitor", "Gym Teacher", "Construction Worker", "Bar Tender"],
+    answer: "Janitor",
+  },
+  {
+    title: "What is the name of the bar Charlie works at?",
+    choices: ["Buddies", "Paddys Pub", "The Fun House", "The Fun House"],
+    answer: "Paddys Pub",
+  },
+  {
+    title: 'In the episode "Charly Work", what does Charly save the bar from?',
+    choices: [
+      "A bad health inspection",
+      "a gang of kids",
+      "being sold for condos",
+      "an employee stealing",
+    ],
+    answer: "A bad health inspection",
+  },
+  {
+    title: "Who is Charlies roomate?",
+    choices: ["Ted", "Frank", "The Bird", "Dennis"],
+    answer: "quotes",
+  },
+  {
+    title: "What is Charlies invention for kittens?",
+    choices: [
+      "Kitten Sushi Bar",
+      "Kitten Dating Site",
+      "Kitten Dubstep Music",
+      "Kitten Mittens",
+    ],
+    answer: "Kitten Mittens",
+  },
+];
 
+console.log(questions[currentQuestionIndex].choices[0]);
+
+function showQuestion() {
+  questionBoxElement.text(questions[currentQuestionIndex].title);
   
- var questions = [
-   {
-     question: "What is Charlie's Job?",
-     answers: [
-       {text: 'Janitor', correct: true },
-       {text: 'Gym Teacher', correct: false },
-       {text: 'Construction Worker', correct: false },
-       {text: 'Bar Tender', correct: false }
-     ]
-   },
-   {
-    question: 'What is the name of the bar Charlie works at?',
-    answers: [
-      {text: 'Buddies', correct: false },
-      {text: 'Paddys Pub', correct: true },
-      {text: 'The Fun House', correct: false },
-      {text: 'Reggies', correct: false }
-    ] 
-  }, 
- ]
-  function showQuestion(question){
-    questionBoxElement.text(question.question)
-    
-    
-  }
-  
-  
-  
-  function showAnswers(){
+
+}
+
+function showAnswers() {
   //for (i = 0; i < 4; i++){
-    btnA.text(this.questions[currentQuestionIndex].answers[0].text)
-    btnB.text(this.questions[currentQuestionIndex].answers[1].text)
-    btnC.text(this.questions[currentQuestionIndex].answers[2].text)
-    btnD.text(this.questions[currentQuestionIndex].answers[3].text)
-    //console.log(this.questions[currentQuestionIndex].answers[i].text)
+  btnA.text(questions[currentQuestionIndex].choices[0]);
+  btnB.text(questions[currentQuestionIndex].choices[1]);
+  btnC.text(questions[currentQuestionIndex].choices[2]);
+  btnD.text(questions[currentQuestionIndex].choices[3]);
+  //console.log(this.questions[currentQuestionIndex].answers[i].text)
   //}
+}
+// here is the timer function
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
   
-    
-  }
+    setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.text(minutes + ":" + seconds);
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+
+
   
-  function checkAnswer(question){
-    
-    
-    
-   
-   
   
+}
+
+function displayScore(){
+
   
 
-  }; 
-    
-    
-   
-
-  
+}
